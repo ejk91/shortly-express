e9df144bffc7021de5c146cc7ffe2283c1889ea1
Enoch Kim <enochkim91@gmail.com>
2017-06-14 17:05:40 -0700

    Updated session functions

--- server/models/session.js
+++ server/models/session.js
@@ -1,7 +1,60 @@
 var db = require('../db');
 var util = require('../lib/utility');
 
-// Write you session database model methods here
+let initializeSession = (agent) => {
+  let salt = util.createSalt();
+  let hash = util.createHash(agent, salt);
+
+  let session = {
+    hash: hash,
+    salt: salt
+  };
+
+  let queryString = 'INSERT INTO sessions SET ?';
+
+  return db.queryAsync(queryString, session)
+    .then((result) => {
+      session.id = result[0].insertId;
+      return session;
+    })
+};
+
+let getSession = (token) => {
+  let queryString = 'SELECT * FROM sessions WHERE hash =?';
+
+  return db.queryAsync(queryString, token)
+    .then((results) => {
+      let session = results[0][0];
+
+      // if there is no session
+      if (!session || !session.user_id) {
+        return session
+      } else {
+      return db.queryAsync('SELECT username FROM users WHERE id = ?', session.user_id)
+        .then((result) => {
+          session.user = result[0][0];
+          return session
+        });
+      }  
+    });
+};
+
+
+let assignSession = (user, sessionHash) => {
+  let queryString = 'UPDATE sessions SET user_id = ? WHERE hash = ?';
+  return db.queryAsync(queryString, [user.id, sessionHash])
+    .return(user);
+}
+
+let destroySession = (token) => {
+  let queryString = 'DELETE FROM sessions WHERE hash = ?';
+  return db.queryAsync(queryString, token);
+}
+
 
 module.exports = {
+  initialize: initializeSession,
+  getSession: getSession,
+  assignSession: assignSession,
+  destroySession: destroySession
 };
